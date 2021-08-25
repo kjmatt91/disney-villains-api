e19d442b9c33e49a311d2c381b3dbb4db2f81f34
kjmatt91 <kjmatt91@gmail.com>
2021-08-24 22:25:04 -0400

    finished the controller villains.js file and created a new index.js and set up npm run dev

--- controllers/villains.js
+++ controllers/villains.js
@@ -2,15 +2,31 @@ const villains = require('../villains')
 const models = require('../models')
 
 const getAllVillains = (request, response) => {
-    const villains = models.villains.findAll()
+  const villains = models.villains.findAll()
 
-    return response.send(heroes)
+  return response.send(villains)
 }
 
 const getVillainBySlug = (request, response) => {
-    const { slug } = request.params
+  const { slug } = request.params
 
-    const foundVillain = villains.filter((villain) => { villain.slug === slug})
+  const foundVillain = villains.filter((villain) => { villain.slug === slug })
 
-    return response.send(foundVillain)
+  return response.send(foundVillain)
 }
+
+const saveNewVillain = (request, response) => {
+  const { name, realname, firstappearance, slug } = request.body
+
+  if (!name || !realname || !firstappearance || !slug) {
+    return response.status(400).send('The following fields are reauired: name, realname, firstappearnace, slug')
+  }
+
+  const newVillain = { name, realname, firstappearance, slug }
+
+  villains.push(newVillain)
+
+  return response.status(201).send(newVillain)
+}
+
+module.exports = { getAllVillains, getVillainBySlug, saveNewVillain }
