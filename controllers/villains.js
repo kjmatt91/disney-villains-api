39bca407aafe6b41635d3551736698700703af11
kjmatt91 <kjmatt91@gmail.com>
2021-08-25 23:13:32 -0400

    Final Commit

--- controllers/villains.js
+++ controllers/villains.js
@@ -1,32 +1,33 @@
-const villains = require('../villains')
 const models = require('../models')
 
-const getAllVillains = (request, response) => {
-  const villains = models.villains.findAll()
+const getAllVillains = async (request, response) => {
+  const villains = await models.villains.findAll()
 
   return response.send(villains)
 }
 
-const getVillainBySlug = (request, response) => {
+const getVillainBySlug = async (request, response) => {
   const { slug } = request.params
 
-  const foundVillain = villains.filter((villain) => { villain.slug === slug })
+  const foundVillain = await models.villains.findOne({ where: { slug } })
+
+  if (!foundVillain) return response.status(404).send('404 Error')
 
   return response.send(foundVillain)
 }
 
-const saveNewVillain = (request, response) => {
+const saveNewVillain = async (request, response) => {
   const { name, realname, firstappearance, slug } = request.body
 
   if (!name || !realname || !firstappearance || !slug) {
     return response.status(400).send('The following fields are reauired: name, realname, firstappearnace, slug')
   }
 
-  const newVillain = { name, realname, firstappearance, slug }
+  const newVillain = await models.villains.create({
+    name, realname, firstappearance, slug
+  })
 
-  villains.push(newVillain)
-
-  return response.status(201).send(newVillain)
+  return response.status(201).send(newVillain, 'createdAt', 'updatedAt')
 }
 
 module.exports = { getAllVillains, getVillainBySlug, saveNewVillain }
